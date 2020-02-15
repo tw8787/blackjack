4858034b3dcb5d3a1e5fd777aa565f29379260b2
Tyler  Wang <tw8787@cs.uw.edu>
2020-02-14 18:46:38 -0800

    Adjusted PlayingCard.java implementation and implemented StandardDeck.java

--- src/play/PlayingCard.java
+++ src/play/PlayingCard.java
@@ -6,6 +6,12 @@ import java.util.Set;
 
 /**
  * <b>PlayingCard</b> is an immutable playing card from a 52-card pack.
+ *
+ * <p>The ranks are represented as their numbers and 11 = Jack, 12 = Queen, and 13 = King, and the
+ * suits are represented as their first letter H(Hearts), D(Diamonds), C(Clubs), S(Spades).
+ *
+ * <p>Typical playing cards are represented as 2S (2 of Spades), 10H (10 of Hearts), and KD (King
+ * of Diamonds).
  */
 public final class PlayingCard {
 
@@ -22,11 +28,11 @@ public final class PlayingCard {
     /**
      * Set of possible suits
      */
-    public final Set<Character> suits = new HashSet<>(Arrays.asList('H', 'D', 'C', 'S'));
+    private static final Set<Character> suits = new HashSet<>(Arrays.asList('H', 'D', 'C', 'S'));
 
-    // Abstraction function:
+    // Abstraction Function:
     // A PlayingCard is uniquely identified by its:
-    //      rank, where 1-10 -> ranks 1-10, 11 -> Jack, 12 -> Queen, 13 -> King
+    //      rank, where 1-10 -> ranks 1-10, 11 -> (J) Jack, 12 -> (Q) Queen, 13 -> (K) King
     //      suit, where H -> Hearts, D -> Diamonds, C -> Clubs, S -> Spades
 
     // Representation Invariant:
@@ -34,6 +40,8 @@ public final class PlayingCard {
     // 0 <= rank <= 13
 
     /**
+     * Constructs a new PlayingCard with rank 'rank' and suit 'suit'
+     *
      * @param rank the rank of this
      * @param suit the suit of this
      * @throws IllegalArgumentException if {@code !suits.contains(suit) || rank < 1 || rank > 13}
@@ -47,11 +55,32 @@ public final class PlayingCard {
         checkRep();
     }
 
+    /**
+     * Returns the string representation of this. Possible examples include "3S", "6H", and "JD".
+     *
+     * @return a string representation of this in the form rank + suit with no whitespace
+     */
+    @Override
+    public String toString() {
+        String result = "" + rank;
+        if(rank == 11) {
+            result = "J";
+        } else if (rank == 12) {
+            result = "Q";
+        } else if (rank == 13) {
+            result = "K";
+        }
+        return result + suit;
+    }
+
+    /**
+     * Checks the the representation invariant.
+     *
+     * @throws AssertionError representation invariant is broken
+     */
     private void checkRep() {
         assert suits.contains(suit);
         assert rank >= 1;
         assert rank <= 13;
     }
-
-
 }
---
+++ src/play/StandardDeck.java
@@ -0,0 +1,141 @@
+package play;
+
+import java.util.*;
+
+/**
+ * <b>StandardDeck</b> is an immutable representation of a shuffled deck of a given number of 52
+ * unique card packs.
+ *
+ * <p>After every card has been retrieved from a StandardDeck, the deck will shuffle its cards as
+ * to continue producing cards.
+ */
+public class StandardDeck {
+
+    /**
+     * List containing the cards in this
+     */
+    public List<PlayingCard> deck;
+
+    /**
+     * Index of the next card to be "removed" from this
+     */
+    private int index;
+
+    /**
+     * Number of 52 card packs in this
+     */
+    private final int numPacks;
+
+    /**
+     * Standard number of packs in a deck;
+     */
+    private static final int standardNumPack = 6;
+
+    /**
+     * Set of possible suits
+     */
+    private static final Set<Character> suits = new HashSet<>(Arrays.asList('H', 'D', 'C', 'S'));
+
+    // Abstraction Function:
+    // A StandardDeck is a collection of 52 (4 suits x 13 ranks) unique PlayingCards
+
+    // Representation Invariant:
+    // deck != null &&
+    // 0 <= index <= deck.size() &&
+    // numPacks >= 1 &&
+    // forall PlayingCard s in deck, s != null &&
+    // forall PlayingCard s in deck, there exists exactly 'numPacks' - 1 other PlayingCards with
+    // the same rank and suit.
+
+    /**
+     * Constructs a new StandardDeck with 6 x 52 unique playing cards
+     */
+    public StandardDeck() {
+        this(standardNumPack);
+    }
+
+    /**
+     * Constructs a new StandardDeck with 'numPacks' x 52 unique playing cards
+     *
+     * @param numPacks number of 52 card packs in this
+     * @throws IllegalArgumentException if {@code numPacks < 1}
+     */
+    public StandardDeck(int numPacks) {
+        if(numPacks < 1) {
+            throw new IllegalArgumentException();
+        }
+        deck = new ArrayList<>();
+        index = 0;
+        this.numPacks = numPacks;
+        for(int i = 0; i < numPacks; i++) {
+            for(int j = 1; j <= 13; j++) {
+                for(char suit : suits) {
+                    deck.add(new PlayingCard(j, suit));
+                }
+            }
+        }
+        Collections.shuffle(deck);
+        checkRep();
+    }
+
+    /**
+     * Draws the next card in this. Shuffles the deck before drawing if every card in this have
+     * already been drawn.
+     *
+     * @return the next PlayingCard in this
+     */
+    public PlayingCard drawNext() {
+        checkRep();
+        if(index == deck.size()) {
+            Collections.shuffle(deck);
+            index = 0;
+        }
+        PlayingCard nextCard = deck.get(index);
+        index++;
+        checkRep();
+        return new PlayingCard(nextCard.rank, nextCard.suit);
+    }
+
+    /**
+     * Checks the the representation invariant.
+     *
+     * @throws AssertionError representation invariant is broken
+     */
+    private void checkRep() {
+        assert deck != null;
+        assert index >= 0;
+        assert index <= deck.size();
+        assert numPacks >= 1;
+        for(PlayingCard card : deck) {
+            assert card != null;
+        }
+        // The following counts the copies of cards in deck, where each index represents a
+        // different card.
+        //
+        // for the card at sortedDeck[i]:
+        // its rank is represented by i mod 13 where 1-10 = ranks 1-10, 11 = Jack, 12 = Queen,
+        // 0 = King
+        //
+        // its suit is represented by i/13 where 0 = Hearts, 1 = Diamonds 2 = Clubs, and 3 = Spades
+        int[] sortedDeck = new int[52];
+        for(PlayingCard card : deck) {
+            int result = 0;
+            if(card.suit == 'D') {
+                result = 13;
+            } else if (card.suit == 'C') {
+                result = 26;
+            } else if (card.suit == 'S') {
+                result = 39;
+            }
+            if(card.rank != 13) {
+                result += card.rank;
+            }
+            sortedDeck[result]++;
+        }
+        // Checks if there are 'numPacks' copies of each card
+        for(int i : sortedDeck) {
+            assert i == numPacks;
+        }
+    }
+
+}
